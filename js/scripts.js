7c6e51789f1774344eb98004bf0353000a8b2d68
Matt Dowdney, matt miller and Zach Spenard <zach@>
2018-04-09 10:39:50 -0700

    Add user logic to adopt pet.

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,39 @@
 //Business Logic
-  function Pet(name, type, color, age, adopted) {
-    this.name = name;
-    this.type = type;
-    this.color = color;
-    this.age = age;
-    this.adopted = false;
-  }
 
-  Pet.prototype.adopt = function() {
-    return this.adopted = true;
-  }
+var petArray = [];
 
+function Pet(name, type, color, age, adopted) {
+  this.name = name;
+  this.type = type;
+  this.color = color;
+  this.age = age;
+  this.adopted = false;
+}
+
+Pet.prototype.adopt = function() {
+  return this.adopted = true;
+  alert("yo!");
+}
+
+Pet.prototype.pushPets = function() {
+  petArray.push(this);
+}
 
 //User Logic
 $(function(){
-  var inputName = $("#input-name").val();
-  var inputType = $("#input-type").val();
-  var inputColor = $("#input-color").val();
-  var inputAge = $("#input-age").val();
-  var pet = new Pet(inputName, inputType, inputColor, inputAge)
-
-  $(".adopt").click(function(){
-    pet.adopt();
-    if (pet.adopted === true) {
-      $(this).hide();
-    }
+  $("#pet-form").submit(function(event){
+    event.preventDefault();
+    var inputName = $("#input-name").val();
+    var inputType = $("#input-type").val();
+    var inputColor = $("#input-color").val();
+    var inputAge = $("#input-age").val();
+    var pet = new Pet(inputName, inputType, inputColor, inputAge)
+    pet.pushPets(this);
+    $("#list-of-pets").append("<li id='" + pet.name + "'>" + pet.name + "<button type='button'class='adopt'>Adopt</button></li>")
+    $(".adopt").click(function(){
+      pet.adopt();
+      // if (pet.adopted === true) {
+        $(this).parent().hide();
+    });
   });
 });
